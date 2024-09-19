b7208629f477c655890439434ea6825bd60309ed
Arundath2001 <arundatharun44@gmail.com>
2024-09-19 18:53:52 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -10,8 +10,22 @@ const fs = require('fs');
 require('dotenv').config();
 
 const app = express();
-app.use(cors({ origin: 'https://www.bhoomikarealestate.com' }));
-app.use('/uploads', express.static('uploads'));app.use(cors());
+const allowedOrigins = ['https://www.bhoomikarealestate.com', 'https://bhoomikarealestate.com'];
+
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    
+    return callback(null, true);
+  },
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true
+}));app.use('/uploads', express.static('uploads'));app.use(cors());
 app.use(express.json());
 
 const pool = new Pool({
